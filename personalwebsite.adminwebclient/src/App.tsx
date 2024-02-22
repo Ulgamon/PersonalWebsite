c8a0f5b81e88193c2bcd6708e3dd32700314f966
Ulgamon <ilicjustin@gmail.com>
2024-02-22 15:35:01 +0100

    shadcn/ui library added.

--- personalwebsite.adminwebclient/src/App.tsx
+++ personalwebsite.adminwebclient/src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-6xl'>CAO NINA!!!!</h1>
+      <h1 className="text-6xl">CAO NINA!!!!</h1>
+      <Button>Hallo</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
