420202432305ef4c1c8500159852b1d1eb555691
Ulgamon <ilicjustin@gmail.com>
2024-02-27 10:51:43 +0100

    Tests for AuthController BlogPosts are finished.

--- PersonalWebsite.Test/Controllers/AuthControllersTest.cs
+++ PersonalWebsite.Test/Controllers/AuthControllersTest.cs
@@ -1,11 +1,15 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using PersonalWebsite.API.Configurations;
 using PersonalWebsite.API.Controllers;
 using PersonalWebsite.API.Data;
+using PersonalWebsite.API.Models.BlogPosts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +20,11 @@ namespace PersonalWebsite.Test.Controllers
 {
     public class AuthControllersTest
     {
-        private readonly ILogger<BlogPostsController> _logger;
+        private readonly ILogger<AuthController> _logger;
         private readonly IMapper _mapper;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
         private async Task<PersonalWebsiteDevelopmentDbContext> GetDatabaseContext()
         {
             var dbOptions = new DbContextOptionsBuilder<PersonalWebsiteDevelopmentDbContext>()
@@ -29,39 +35,39 @@ namespace PersonalWebsite.Test.Controllers
             await databaseContext.Database.EnsureCreatedAsync();
             try
             {
-                // Create Role
-                await databaseContext.Roles.AddAsync(new IdentityRole
-                {
-                    Name = "User",
-                    NormalizedName = "USER",
-                    Id = "f2a04be0-60e2-4835-b0ea-4ac09e8449c5"
-                });
-                await databaseContext.Roles.AddAsync(new IdentityRole
-                {
-                    Name = "Administrator",
-                    NormalizedName = "ADMINISTRATOR",
-                    Id = "5424f84e-ebad-491f-bf86-96903dbaf476"
-                });
-                // Create ONE DEFAULT USER FOR INMEMORYDATABASE
-                var hasher = new PasswordHasher<ApplicationUser>();
+                //// Create Role
+                //await databaseContext.Roles.AddAsync(new IdentityRole
+                //{
+                //    Name = "User",
+                //    NormalizedName = "USER",
+                //    Id = "f2a04be0-60e2-4835-b0ea-4ac09e8449c5"
+                //});
+                //await databaseContext.Roles.AddAsync(new IdentityRole
+                //{
+                //    Name = "Administrator",
+                //    NormalizedName = "ADMINISTRATOR",
+                //    Id = "5424f84e-ebad-491f-bf86-96903dbaf476"
+                //});
+                //// Create ONE DEFAULT USER FOR INMEMORYDATABASE
+                //var hasher = new PasswordHasher<ApplicationUser>();
                
-                await databaseContext.Users.AddAsync(new ApplicationUser
-                {
-                    Id = "37ca6d9e-a698-4cc8-8655-9ad6d1670593",
-                    Email = "test@gmail.com",
-                    NormalizedEmail = "TEST@GMAIL.COM",
-                    UserName = "test@gmail.com",
-                    PasswordHash = hasher.HashPassword(null, "P@ssword123")
-                });
-                await databaseContext.SaveChangesAsync();
+                //await databaseContext.Users.AddAsync(new ApplicationUser
+                //{
+                //    Id = "37ca6d9e-a698-4cc8-8655-9ad6d1670593",
+                //    Email = "test@gmail.com",
+                //    NormalizedEmail = "TEST@GMAIL.COM",
+                //    UserName = "test@gmail.com",
+                //    PasswordHash = hasher.HashPassword(null, "P@ssword123")
+                //});
+                //await databaseContext.SaveChangesAsync();
 
-                await databaseContext.UserRoles.AddAsync(new IdentityUserRole<string>
-                {
-                    RoleId = "5424f84e-ebad-491f-bf86-96903dbaf476",
-                    UserId = "37ca6d9e-a698-4cc8-8655-9ad6d1670593"
-                });
+                //await databaseContext.UserRoles.AddAsync(new IdentityUserRole<string>
+                //{
+                //    RoleId = "5424f84e-ebad-491f-bf86-96903dbaf476",
+                //    UserId = "37ca6d9e-a698-4cc8-8655-9ad6d1670593"
+                //});
 
-                await databaseContext.SaveChangesAsync();
+                //await databaseContext.SaveChangesAsync();
 
                 // CREATE 10 Categories
                 if (await databaseContext.Categories.CountAsync() <= 0)
@@ -96,6 +102,7 @@ namespace PersonalWebsite.Test.Controllers
                                 Title = $"Good BlogPost {i}",
                                 ImgUrl = $"http://localhost:1333/img{i}",
                                 UserId = "toja",
+                                Published = true,
                                 Categories = new List<Category>
                                 {
                                     item
@@ -105,6 +112,27 @@ namespace PersonalWebsite.Test.Controllers
                     }
                     await databaseContext.SaveChangesAsync();
                 }
+
+                // Add another one BlogPost that is not Published for the tests to be correct
+                int num = 55;
+                var category = await databaseContext.Categories.FindAsync(10);
+                await databaseContext.BlogPosts.AddAsync(
+                           new BlogPost
+                           {
+                               Id = num,
+                               BlogMdText = $"# Hello World {num}",
+                               Title = $"Good BlogPost {num}",
+                               ImgUrl = $"http://localhost:1333/img{num}",
+                               UserId = "toja",
+                               Categories = new List<Category>
+                               {
+                                    category
+                               }
+                           }
+                       );
+
+                await databaseContext.SaveChangesAsync();
+
             }
             catch (Exception ex)
             {
@@ -118,9 +146,23 @@ namespace PersonalWebsite.Test.Controllers
         {
 
             // Mock ILogger Interface
-            var loggerMock = new Mock<ILogger<BlogPostsController>>();
+            var loggerMock = new Mock<ILogger<AuthController>>();
             _logger = loggerMock.Object;
 
+            var userManagerMock = new Mock<UserManager<ApplicationUser>>(
+                new Mock<IUserStore<ApplicationUser>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                new IUserValidator<ApplicationUser>[0],
+                new IPasswordValidator<ApplicationUser>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
+            _userManager = userManagerMock.Object;
+
+            var configurationMock = new Mock<IConfiguration>();
+            _configuration = configurationMock.Object;
             // Add userManager
             //_userManager = new UserManager<ApplicationUser>();
 
@@ -132,6 +174,99 @@ namespace PersonalWebsite.Test.Controllers
                 cfg.AddProfile<MapperConfig>();
             });
             _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async void GET_BlogPosts_Authenticated()
+        {
+            // Arrange
+            var context = await GetDatabaseContext();
+            AuthController controller = new AuthController(_userManager, context, _logger, _mapper, _configuration);
+
+            // Act
+            ActionResult<PaginateBlogPostsDto> response = await controller.GetBlogPosts();
+
+            // Assert
+            OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(response.Result);
+            PaginateBlogPostsDto model = Assert.IsType<PaginateBlogPostsDto>(okObjectResult.Value);
+            Assert.NotNull(model);
+            Assert.Equal(3, model.BlogPostsDtos.Count);
+            Assert.True(model.HasNext);
+            Assert.False(model.HasPrev);
+        }
+
+        [Fact]
+        public async void GET_BlogPosts_WithQueryParameter_ThirdPage()
+        {
+            // Arrange
+            var context = await GetDatabaseContext();
+            AuthController controller = new AuthController(_userManager, context, _logger, _mapper, _configuration);
+
+            // Act
+            ActionResult<PaginateBlogPostsDto> response = await controller.GetBlogPosts(3, 3);
+
+            // Assert
+            OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(response.Result);
+            PaginateBlogPostsDto model = Assert.IsType<PaginateBlogPostsDto>(okObjectResult.Value);
+            Assert.NotNull(model);
+            Assert.Equal(3, model.BlogPostsDtos.Count);
+            Assert.Equal(4, model.BlogPostsDtos.ElementAt(0).Id);
+            Assert.Equal(3, model.BlogPostsDtos.ElementAt(1).Id);
+            Assert.Equal(2, model.BlogPostsDtos.ElementAt(2).Id);
+            Assert.True(model.HasNext);
+            Assert.True(model.HasPrev);
+        }
+
+        [Fact]
+        public async void GET_BlogPosts_WithQueryParameter_FourthPage()
+        {
+            // Arrange
+            var context = await GetDatabaseContext();
+            AuthController controller = new AuthController(_userManager, context, _logger, _mapper, _configuration);
+
+            // Act
+            ActionResult<PaginateBlogPostsDto> response = await controller.GetBlogPosts(3, 4);
+
+            // Assert
+            OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(response.Result);
+            PaginateBlogPostsDto model = Assert.IsType<PaginateBlogPostsDto>(okObjectResult.Value);
+            Assert.NotNull(model);
+            Assert.Equal(2, model.BlogPostsDtos.Count);
+            Assert.Equal(1, model.BlogPostsDtos.ElementAt(0).Id);
+            Assert.False(model.HasNext);
+            Assert.True(model.HasPrev);
+        }
+
+        [Fact]
+        public async void GET_BlogPosts_WithQueryParameter_FifthPage()
+        {
+            // Arrange
+            var context = await GetDatabaseContext();
+            AuthController controller = new AuthController(_userManager, context, _logger, _mapper, _configuration);
+
+            // Act
+            ActionResult<PaginateBlogPostsDto> response = await controller.GetBlogPosts(3, 5);
+
+            // Assert
+            BadRequestObjectResult errorObjectResult = Assert.IsType<BadRequestObjectResult>(response.Result);
+            Assert.Equal("Out of range page and/or size parameters.", errorObjectResult.Value);
+
+        }
+
+        [Fact]
+        public async void GET_BlogPosts_WithQueryParameter_InvalidPageAndSize()
+        {
+            // Arrange
+            var context = await GetDatabaseContext();
+            AuthController controller = new AuthController(_userManager, context, _logger, _mapper, _configuration);
+
+            // Act
+            ActionResult<PaginateBlogPostsDto> response = await controller.GetBlogPosts(-3, -5);
+
+            // Assert
+            BadRequestObjectResult errorObjectResult = Assert.IsType<BadRequestObjectResult>(response.Result);
+            Assert.Equal("Invalid size and/or page params should be size >= 1 and page >= 1.", errorObjectResult.Value);
+
         }
     }
 }
