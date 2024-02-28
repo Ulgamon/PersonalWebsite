a0074c880ec852dea799d1890684e4f07f9b8330
Ulgamon <ilicjustin@gmail.com>
2024-02-28 10:13:03 +0100

    Search feature is being added.

---
+++ PersonalWebsite.API/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalWebsite.API.Data;
+using PersonalWebsite.API.Models.BlogPosts;
+
+namespace PersonalWebsite.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly PersonalWebsiteDevelopmentDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ILogger<SearchController> _logger;
+
+
+        public SearchController(PersonalWebsiteDevelopmentDbContext context, IMapper mapper, ILogger<SearchController> logger)
+        {
+            _context = context;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult<PaginateBlogPostsDto>> SearchBlogPosts(string q, [FromQuery] int[] categoryids)
+        //{
+        //    return Ok();
+        //}
+    }
+}
