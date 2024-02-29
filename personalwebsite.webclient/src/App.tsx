eaabe697dfa7fa25b3bdcac1a4cd1397821613d6
Ulgamon <ilicjustin@gmail.com>
2024-03-01 00:35:15 +0100

    ThemeContextProvider created and react-icons library installed.

--- personalwebsite.webclient/src/App.tsx
+++ personalwebsite.webclient/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-blue-500">Tojica</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<>HAHAHAHAH</>} />
+            <Route path="blog/:blogId" element={<>Blog With ID</>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
