2e31ee264d151c6b28ff76eea3779245c4661ed1
Ulgamon <ilicjustin@gmail.com>
2024-04-02 13:37:54 +0200

    Sheet lilnks are now working properly.

--- personalwebsite.webclient/src/components/sheet-nav-link/SheetNavLink.tsx
+++ personalwebsite.webclient/src/components/sheet-nav-link/SheetNavLink.tsx
@@ -1,3 +1,4 @@
+import { NavLink, useLocation } from "react-router-dom";
 import { Link } from "react-scroll";
 
 interface ISheetNavLinkProps {
@@ -6,20 +7,35 @@ interface ISheetNavLinkProps {
 }
 
 const SheetNavLink = ({ to, children }: ISheetNavLinkProps) => {
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    return (
+      <Link
+        className={
+          "font-semibold w-full cursor-pointer py-2 ps-4 block my-1 rounded-xl hover:bg-themeOrange hover:dark:bg-themeBlue"
+        }
+        activeClass=" bg-themeOrange dark:bg-themeBlue"
+        to={to}
+        smooth={true}
+        spy={true}
+        offset={-90}
+        duration={400}
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <Link
+    <NavLink
+      to={"/"}
       className={
         "font-semibold w-full cursor-pointer py-2 ps-4 block my-1 rounded-xl hover:bg-themeOrange hover:dark:bg-themeBlue"
       }
-      activeClass=" bg-themeOrange dark:bg-themeBlue"
-      to={to}
-      smooth={true}
-      spy={true}
-      offset={-90}
-      duration={400}
     >
       {children}
-    </Link>
+    </NavLink>
   );
 };
 
